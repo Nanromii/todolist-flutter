15a796bc612924cf1f36bd855c7d1823140dee49
Nanromii <vuquangnam2005@gmail.com>
2026-06-21 15:56:17 +0700

    add event for add task button

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,43 @@ class MyApp extends StatelessWidget {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => {},
+        onPressed: () => {
+          showModalBottomSheet(
+            backgroundColor: Colors.grey[400],
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+            ),
+            isScrollControlled: true,
+            context: context,
+            builder: (BuildContext context) {
+              return Padding(
+                padding: MediaQuery.of(context).viewInsets,
+                child: SingleChildScrollView(
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                  child: Column(
+                    children: [
+                      TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Your task',
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      SizedBox(
+                        width: double.infinity,
+                        height: 50,
+                        child: ElevatedButton(
+                          onPressed: () => {},
+                          child: Text("Add Task"),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
+        },
         child: Icon(Icons.add, size: 40),
       ),
     );
